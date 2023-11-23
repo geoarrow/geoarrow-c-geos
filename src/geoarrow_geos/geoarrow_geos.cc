29c47bca22c79bab279380bb0706dac3177cc89c
Dewey Dunnington <dewey@fishandwhistle.net>
2023-11-22 22:11:25 -0400

    first commit

---
+++ src/geoarrow_geos/geoarrow_geos.cc
@@ -0,0 +1,9 @@
+
+#include <geos_c.h>
+#include <geoarrow.h>
+
+#include "geoarrow_geos.h"
+
+const char* GeoArrowGEOSVersionGEOS(void) { return nullptr; }
+
+const char* GeoArrowGEOSVersionGeoArrow(void) { return nullptr; }
---
+++ src/geoarrow_geos/geoarrow_geos_test.cc
@@ -0,0 +1,8 @@
+
+#include <gtest/gtest.h>
+
+#include "geoarrow_geos.h"
+
+TEST(GeoArrowGEOSTest, Basic) {
+    ASSERT_EQ(1, 1);
+}
