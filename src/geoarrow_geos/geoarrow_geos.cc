d7c53ec0a55228c2dc082b6d2c42e610a8155a35
Dewey Dunnington <dewey@fishandwhistle.net>
2023-11-22 22:30:26 -0400

    version testing

--- src/geoarrow_geos/geoarrow_geos.cc
+++ src/geoarrow_geos/geoarrow_geos.cc
@@ -1,9 +1,11 @@
 
+#define GEOS_USE_ONLY_R_API
+
 #include <geos_c.h>
 #include <geoarrow.h>
 
 #include "geoarrow_geos.h"
 
-const char* GeoArrowGEOSVersionGEOS(void) { return nullptr; }
+const char* GeoArrowGEOSVersionGEOS(void) { return GEOSversion(); }
 
-const char* GeoArrowGEOSVersionGeoArrow(void) { return nullptr; }
+const char* GeoArrowGEOSVersionGeoArrow(void) { return GeoArrowVersion(); }
--- src/geoarrow_geos/geoarrow_geos_test.cc
+++ src/geoarrow_geos/geoarrow_geos_test.cc
@@ -3,6 +3,7 @@
 
 #include "geoarrow_geos.h"
 
-TEST(GeoArrowGEOSTest, Basic) {
-    ASSERT_EQ(1, 1);
+TEST(GeoArrowGEOSTest, TestVersions) {
+  ASSERT_EQ(std::string(GeoArrowGEOSVersionGEOS()).substr(0, 1), "3");
+  ASSERT_STREQ(GeoArrowGEOSVersionGeoArrow(), "0.2.0-SNAPSHOT");
 }
